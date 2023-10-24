75fae9ffa72438bb260dc38c523b31a98f797039
Mathieu Lefebvre <mathieu@eosnation.io>
2023-10-24 14:42:42 -0400

    update test

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,5 +1,4 @@
 import { describe, expect, it } from 'bun:test';
-import { banner } from "../src/banner";
 import { generateApp } from '../src/index';
 
 const app = generateApp();
@@ -8,11 +7,6 @@ describe('Index page (/)', () => {
     it('Should return 200 Response', async () => {
         const res = await app.request('/');
         expect(res.status).toBe(200);
-    });
-
-    it('Should have the banner as the body', async () => {
-        const res = await app.request('/');
-        expect(await res.text()).toBe(banner());
     });
 });
 
