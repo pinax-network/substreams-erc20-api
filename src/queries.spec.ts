f7f142ec08b9b23e72b2476735a34df874e841a3
Mathieu Lefebvre <mathieu@eosnation.io>
2023-10-25 15:07:49 -0400

    add more tests

--- src/queries.spec.ts
+++ src/queries.spec.ts
@@ -5,12 +5,27 @@ import { getContracts, getChain, getTotalSupply, getBalanceChanges } from "./que
 
 const chain = "eth";
 const address = 'dac17f958d2ee523a2206206994597c13d831ec7'
-const limit = 1;
+const limit = '1';
+const symbol = "USDT";
+const name = "Tether USD";
+const greater_or_equals_by_timestamp = "1697587200";
+const less_or_equals_by_timestamp = "1697587100";
+const transaction_id = "ab3612eed62a184eed2ae86bcad766183019cf40f82e5316f4d7c4e61f4baa44"
+
+//Test Contract 
 test("getContracts", () => {
     expect(getContracts(new URLSearchParams({ chain, address })).replace(/\s+/g, ''))
         .toBe(`SELECT * FROM Contracts  JOIN blocks ON blocks.block_id = Contracts.block_id WHERE(chain == '${chain}' AND address == '${address}') ORDER BY block_number DESC  LIMIT 1 `.replace(/\s+/g, ''));
 });
 
+//Test Contract with optionals options
+test("getContracts Optional", () => {
+    expect(getContracts(new URLSearchParams({ chain, address, symbol, greater_or_equals_by_timestamp, less_or_equals_by_timestamp, name, limit })).replace(/\s+/g, ''))
+        .toBe(`SELECT * FROM Contracts  JOIN blocks ON blocks.block_id = Contracts.block_id WHERE(chain == '${chain}' AND address == '${address}' AND symbol == '${symbol}' AND name =='${name}' AND toUnixTimestamp(timestamp) >= ${greater_or_equals_by_timestamp} AND toUnixTimestamp(timestamp) <= ${less_or_equals_by_timestamp}) ORDER BY block_number DESC  LIMIT ${limit} `.replace(/\s+/g, ''));
+});
+
+
+//Test TotalSupply 
 test("getTotalSupply", () => {
     expect(getTotalSupply(new URLSearchParams({ chain, address })).replace(/\s+/g, ''))
         .toBe(`SELECT 
@@ -31,6 +46,32 @@ test("getTotalSupply", () => {
         ORDER BY block_number 
         DESC  LIMIT 1 `.replace(/\s+/g, ''));
 });
+
+//Test TotalSupply 
+test("getTotalSupply optional", () => {
+    expect(getTotalSupply(new URLSearchParams({ chain, address, symbol, greater_or_equals_by_timestamp, less_or_equals_by_timestamp, name, limit })).replace(/\s+/g, ''))
+        .toBe(`SELECT 
+        TotalSupply.address as address,
+        TotalSupply.supply as supply,
+        TotalSupply.id as id,
+        block_number,
+        TotalSupply.module_hash as module_hash,
+        TotalSupply.chain as chain,
+        Contracts.name as name,
+        Contracts.symbol as symbol,
+        Contracts.decimals as decimals,
+        timestamp,
+        FROM TotalSupply
+        JOIN blocks ON blocks.block_id = TotalSupply.block_id 
+        LEFT JOIN Contracts ON Contracts.address = TotalSupply.address 
+        WHERE(TotalSupply.chain == '${chain}' AND TotalSupply.address == '${address}' AND toUnixTimestamp(timestamp) >= ${greater_or_equals_by_timestamp} AND toUnixTimestamp(timestamp) <= ${less_or_equals_by_timestamp}  AND symbol == '${symbol}' AND name == '${name}')
+        ORDER BY block_number 
+        DESC  LIMIT ${limit} `.replace(/\s+/g, ''));
+});
+
+
+
+//Test BalanceChanges 
 test("getBalanceChanges", () => {
     expect(getBalanceChanges(new URLSearchParams({ chain, owner: address })).replace(/\s+/g, ''))
         .toBe(`SELECT balance_changes.contract as contract,
@@ -51,6 +92,32 @@ test("getBalanceChanges", () => {
         LEFT JOIN Contracts ON Contracts.address = balance_changes.contract 
         WHERE(chain == '${chain}' AND owner == '${address}') ORDER BY block_number DESC  LIMIT 1 `.replace(/\s+/g, ''))
 });
+
+
+
+
+//Test BalanceChanges Optional
+test("getBalanceChanges Optional", () => {
+    expect(getBalanceChanges(new URLSearchParams({ chain, owner: address, transaction_id, greater_or_equals_by_timestamp, less_or_equals_by_timestamp, limit })).replace(/\s+/g, ''))
+        .toBe(`SELECT balance_changes.contract as contract,
+        Contracts.name as name,
+        Contracts.symbol as symbol,
+        Contracts.decimals as decimals,
+        balance_changes.owner as owner,
+        balance_changes.old_balance as old_balance,
+        balance_changes.new_balance as new_balance,
+        balance_changes.transaction_id as transaction_id,
+        balance_changes.id as id,
+        balance_changes.module_hash as module_hash,
+        balance_changes.chain as chain,
+        block_number,
+        timestamp
+        FROM balance_changes
+        JOIN blocks ON blocks.block_id = balance_changes.block_id 
+        LEFT JOIN Contracts ON Contracts.address = balance_changes.contract 
+        WHERE(chain == '${chain}' AND owner == '${address}' AND balance_changes.transaction_id == '${transaction_id}' AND toUnixTimestamp(timestamp) >= ${greater_or_equals_by_timestamp} AND  toUnixTimestamp(timestamp) <= ${less_or_equals_by_timestamp}) ORDER BY block_number DESC  LIMIT ${limit} `.replace(/\s+/g, ''))
+});
+
 
 test("getChain", () => {
     expect(getChain()).toBe(`SELECT DISTINCT chain FROM module_hashes`);
--- src/utils.spec.ts
+++ src/utils.spec.ts
@@ -1,5 +1,5 @@
 import { expect, test } from "bun:test";
-import { formatAddress, checkValidAddress, getAddress } from "./utils.js";
+import { formatAddress, checkValidAddress, getAddress, parseBlockId, parseTimestamp } from "./utils.js";
 
 const address = "0xdac17f958d2ee523a2206206994597c13d831ec7";
 
@@ -14,9 +14,21 @@ test("checkValidAddress", () => {
     expect(() => checkValidAddress("foobar")).toThrow("Invalid address");
 });
 
+test("parseBlockId", () => {
+    expect(parseBlockId("0x123") as string).toBe("123");
+});
+
+test("parseTimestamp", () => {
+    expect(parseTimestamp("1697587100")).toBe(1697587100);
+    expect(parseTimestamp("1697587100000")).toBe(1697587100);
+    expect(parseTimestamp("awdawd")).toBeNaN();
+    expect(parseTimestamp(null)).toBeUndefined();
+});
+
+
 test("getAddress", () => {
-    expect(() => getAddress(new URLSearchParams({address: address}), "address", false)).not.toThrow();
-    expect(() => getAddress(new URLSearchParams({address: address}), "address", true)).not.toThrow();
-    expect(() => getAddress(new URLSearchParams({address: ""}), "address", true)).toThrow("Missing [address] parameter");
-    expect(() => getAddress(new URLSearchParams({address: "foobar"}), "address")).toThrow("Invalid address");
+    expect(() => getAddress(new URLSearchParams({ address: address }), "address", false)).not.toThrow();
+    expect(() => getAddress(new URLSearchParams({ address: address }), "address", true)).not.toThrow();
+    expect(() => getAddress(new URLSearchParams({ address: "" }), "address", true)).toThrow("Missing [address] parameter");
+    expect(() => getAddress(new URLSearchParams({ address: "foobar" }), "address")).toThrow("Invalid address");
 });
