54b1851a3fe6156d979eb16b9234260d78b411f1
Denis Carriere <carriere.denis@gmail.com>
2023-10-25 12:20:20 -0400

    update tests to use `blocks`

--- src/queries.spec.ts
+++ src/queries.spec.ts
@@ -8,17 +8,17 @@ const address = 'dac17f958d2ee523a2206206994597c13d831ec7'
 
 test("getContracts", () => {
     expect(getContracts(new URLSearchParams({ chain, address })))
-        .toBe(`SELECT * FROM Contracts JOIN block ON block.block_id = Contracts.block_id WHERE (chain == '${chain}' AND address == '${address}') ORDER BY block_number DESC LIMIT 1`);
+        .toBe(`SELECT * FROM Contracts JOIN blocks ON blocks.block_id = Contracts.block_id WHERE (chain == '${chain}' AND address == '${address}') ORDER BY block_number DESC LIMIT 1`);
 });
 
 test("getTotalSupply", () => {
     expect(getTotalSupply(new URLSearchParams({ chain, address })))
-        .toBe(`SELECT * FROM TotalSupply JOIN block ON block.block_id = TotalSupply.block_id WHERE (chain == '${chain}' AND address == '${address}') ORDER BY block_number DESC LIMIT 1`);
+        .toBe(`SELECT * FROM TotalSupply JOIN blocks ON blocks.block_id = TotalSupply.block_id WHERE (chain == '${chain}' AND address == '${address}') ORDER BY block_number DESC LIMIT 1`);
 });
 
 test("getBalanceChanges", () => {
     expect(getBalanceChanges(new URLSearchParams({ chain, owner: address })))
-        .toBe(`SELECT * FROM balance_changes JOIN block ON block.block_id = balance_changes.block_id WHERE (chain == '${chain}' AND owner == '${address}') ORDER BY block_number DESC LIMIT 1`);
+        .toBe(`SELECT * FROM balance_changes JOIN blocks ON blocks.block_id = balance_changes.block_id WHERE (chain == '${chain}' AND owner == '${address}') ORDER BY block_number DESC LIMIT 1`);
 });
 
 test("getChain", () => {
